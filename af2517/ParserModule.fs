b00f4afcba8b1c8829f9a647899abc0b9e83372b
AlbertFletaMoya <albert.fleta17@imperial.ac.uk>
2020-02-17 15:29:54 +0000

    Update ParserModule.fs

--- af2517/ParserModule.fs
+++ af2517/ParserModule.fs
@@ -1,26 +1,46 @@
 module ParserModule
 
 let test() = printfn "sample top level function in ParserModule" 
+type AdditiveOperator = Sum of char list | Subtraction of char list 
+type MultiplicativeOperator = Multiplication of char list | Division of char list 
+// <item> ::= <numeric-literal> | <string-literal> | <bracketed-additive-exp> | <if-exp>
+
+// <applicative-exp> ::= <item> | <item> <applicative-exp>
+
+// <multiplicative-exp> ::= <applicative-exp> | <applicative-exp> <multiplicative-op> <multiplicative-exp>
+
+// <additive-exp> ::= <multiplicative-exp> | <multiplicative-exp> <additive-op> <additive-exp>
+
+// <multiplicative-op> ::= "*" | "/"
+
+// <additive-op> ::= "+ | "-"
+
+//ordered from high to low precedence 
+type Precedence = 
+    | AdditiveExpression of Precedence //level 4
+    | MultiplicativeExpression of Precedence //level 3
+    | ApplicativeExpression of Precedence  //level 2
+    | BracketedAdditiveExpression of Precedence //level 1 behaves like additive exp
+    | IfExpression of Precedence // level 1 
+    | Other of char list // level 1 int string params etc
+    // how to implement function definition in here?
 
 type BuiltInType = 
     | Add 
     | Sub 
     | Mult 
     | Div
+    | Mod
     | Test 
-    | IntEQ
-    | Bigger
-    | BiggerOrEqual
-    | Smaller 
-    | SmallerOrEqual
-    | StringEQ
-    | StringToCharList
-    | CharListToString
-
-type LitType = 
-    | Int of int 
-    | String of string 
-
+    | Equal //works for strings ints and nulls 
+    | Explode 
+    | Implode 
+    | P //creates a pair 
+    | PFst 
+    | PSnd
+    | IsPair
+    | IfThenElse  
+    
 type AST = 
     | FuncDefExp of FuncDefExpType 
     | Lambda of LambdaType: (char list)*Body:AST
@@ -30,8 +50,13 @@ type AST =
     | Null 
     | Literal of LitType 
     | BuiltInFunc of BuiltInType
-    | Param of char list //when a parameter is given for funcapp whose type is unknown bc
-    // it was previously defined 
+    | Param of char list
+
+and LitType = 
+    | Int of int 
+    | String of string
+    | True of AST //make it a named function with Lambda ("A",Lambda ("B",Var "A" ))
+    | False of AST // Lambda ("A",Lambda ("B",Var "A" ))
 //    | Combinator of CombinatorType do later 
 
 //FuncApp(FuncApp(Plus, exp1), exp2) ==> exp1 + exp2
