aced31cc47becde111d2bc2c8a5429dedeefd330
AlbertFletaMoya <albert.fleta17@imperial.ac.uk>
2020-02-18 13:31:21 +0000

    Update ParserModule

--- af2517/ParserModule.fs
+++ af2517/ParserModule.fs
@@ -1,29 +1,14 @@
 module ParserModule
 
 let test() = printfn "sample top level function in ParserModule" 
-type AdditiveOperator = Sum of char list | Subtraction of char list 
-type MultiplicativeOperator = Multiplication of char list | Division of char list 
-// <item> ::= <numeric-literal> | <string-literal> | <bracketed-additive-exp> | <if-exp>
-
-// <applicative-exp> ::= <item> | <item> <applicative-exp>
-
-// <multiplicative-exp> ::= <applicative-exp> | <applicative-exp> <multiplicative-op> <multiplicative-exp>
-
-// <additive-exp> ::= <multiplicative-exp> | <multiplicative-exp> <additive-op> <additive-exp>
-
-// <multiplicative-op> ::= "*" | "/"
-
-// <additive-op> ::= "+ | "-"
-
-//ordered from high to low precedence 
-type Precedence = 
-    | AdditiveExpression of Precedence //level 4
-    | MultiplicativeExpression of Precedence //level 3
-    | ApplicativeExpression of Precedence  //level 2
-    | BracketedAdditiveExpression of Precedence //level 1 behaves like additive exp
-    | IfExpression of Precedence // level 1 
-    | Other of char list // level 1 int string params etc
-    // how to implement function definition in here?
+type Bracket = Round |Square 
+type Keyword = LET |RIGHTARROW |Extension of string 
+type Token = 
+    | Other of char list  
+    | Bracket of Bracket
+    | Keyword of Keyword
+    | IntToken of int  
+    | StringToken of char list 
 
 type BuiltInType = 
     | Add 
@@ -42,25 +27,54 @@ type BuiltInType =
     | IfThenElse  
     
 type AST = 
-    | FuncDefExp of FuncDefExpType:string*AST
-    | Lambda of LambdaType: (char list)*Body:AST
+    | FuncDefExp of FuncDefExpType:AST //FuncApp(Lambda("f", x*(f x)),Lambda("x",x+1))
+    | Lambda of LambdaType:(char list)*Body:AST
     | Var of char list //only valid in lambdas 
     | Funcapp of AST*AST
     | Pair of AST*AST 
     | Null 
     | Literal of LitType 
     | BuiltInFunc of BuiltInType
-    | Param of char list
 
 and LitType = 
     | Int of int 
-    | String of string
+    | String of char list 
     | True of AST //make it a named function with Lambda ("A",Lambda ("B",Var "A" ))
-    | False of AST // Lambda ("A",Lambda ("B",Var "A" ))
-//    | Combinator of CombinatorType do later 
+    | False of AST 
 
-//FuncApp(FuncApp(Plus, exp1), exp2) ==> exp1 + exp2
+ //1+2 -> AddMult(JustAppExp(AppExpItem(Literal(Int 1))), JustMultExp(JustAppExp(AppExpItem(Literal(Int 2)))))
+type AppExp = AppExpItem of AST | AppExpExp of AST*AppExp
+type MultExp = JustAppExp of AppExp | MultApp of AppExp*MultExp
+type AddExp = JustMultExp of MultExp | AddMult of MultExp*AddExp  
+type ASTBRACKETS = BRA of ASTBRACKETS | EXP of char list
 
-//fun f -> fun x -> fun y -> f y x ==>
-//Lambda("f", Lambda( "x", Lambda("y",FuncApp(FuncApp (Var "f", Var "y"), Var "x"))))
+let (|PMATCH|_|) (tok: Token) (tokLst: Result<Token list, Token list>) = 
+    match tokLst with
+    | Ok []  -> Error []
+    | Ok (s :: rest) when s = tok -> Ok rest
+    | Ok lst -> Error lst
+    | Error lst -> Error lst
+    |> Some
 
+let (|PITEM|_|) (tokLst: Result<Token list, Token list>)  =
+    match tokLst with
+    | Ok [] -> Error []
+    | Ok (Other s :: rest) ->  Ok (Var s, rest)
+    | Ok (IntToken s:: rest) -> Ok (Literal (Int s) , rest)
+    | Ok (StringToken s::rest) -> Ok (Literal (String s), rest) //choose what to return 
+    | Ok lst -> Error lst
+    | Error lst -> Error lst
+    |> Some
+//one recursive function needed for every precedence level 
+//[Other "f"; Other "x"] -> AppExpExp(f,AppExpItem(x))
+//<applicative-exp> ::= <item> | <item> <applicative-exp>
+let rec (|PAPPEXP|_|)(inp: Result<Token list, Token list>):((AppExp option * Result<Token list, Token list>) option) =
+    match inp with
+    // TWO CASES IF PAPP MATCHES AGAIN THEN APPEXPEXP ELSE APPEXPITEM
+    | PITEM (Ok(s, lst)) -> Some ((Some (AppExpItem s)), Ok lst) 
+    | Error msg -> Some (None, Error msg)
+    //| PMATCH "(" (PBRACKETS(Some ast,PMATCH ")" (inp'))) -> Some ( Some (BRA ast ), inp') don't think I need it here
+    | Ok lst ->  failwithf "What? Can't happen"
+    | _ ->  failwithf "What? Can't happen"
+
+let PappExp = (|PAPPEXP|_|)
--- af2517/Program.fs
+++ af2517/Program.fs
@@ -3,10 +3,14 @@
 open System
 //If FSI not finding the module do in the FSI #load ParserModule
 open ParserModule
-
+let print x = printfn "%A" x
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    print <| PappExp (Ok [IntToken 33])
     test()
     0 // return an integer exit code
+
+
+
