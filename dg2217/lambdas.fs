5bde7dd3ded8f3f2e25c7dd50a6bd391ac568bbf
danigr99727 <37747867+danigr99727@users.noreply.github.com>
2020-02-24 17:20:18 +0000

    math added to AST

--- dg2217/lambdas.fs
+++ dg2217/lambdas.fs
@@ -1,12 +1,9 @@
 module Lambdas
 
+type MathType = |Add|Sub|Mult|Div|Mod
+
 type BuiltInType = 
-    | Add
-    | Sub 
-    | Mult 
-    | Div 
-    | Mod 
-    | Test
+    | Math of MathType
     | Equal //works for strings ints and nulls 
     | Explode 
     | Implode 
@@ -15,6 +12,7 @@ type BuiltInType =
     | PSnd
     | IsPair
     | IfThenElse  
+
 
 type AST = 
     | FuncDefExp of FuncDefExpType:char list*AST*AST 
@@ -61,28 +59,24 @@ let rec implode lst =
 
 let rec explode str =
     match str with
-    | Literal(String ([])) -> Null
-    | Literal(String s) -> Pair(Literal(String([s.[0]])), explode (Literal(String(s.[1..]))))
+    | [] -> Null
+    | hd::tl -> Pair(Literal(String([hd])), explode tl)
 
 concat (Literal(String['a';'b'])) (Literal(String ['c';'d']))
-explode (Literal(String ['a';'b';'c';'d']))
+explode ['a';'b';'c';'d']
 implode (Pair(Literal (String ['a']),Pair(Literal (String ['b']),Pair (Literal (String ['c']),Pair (Literal (String ['d']),Null)))))
 
 
 let execArithmetic func A B =
-    match (A,B) with 
-    |(Literal (Int valueA),Literal(Int valueB))->
-        match func with
-        //| P -> Pair()
-        | Add -> Literal(Int(valueA+valueB))
-        | Sub -> Literal(Int(valueA-valueB))
-        | Mult -> Literal(Int(valueA*valueB))
-        | Div -> Literal(Int(valueA/valueB))
-        | Mod -> Literal(Int(valueA%valueB))
-        | Equal -> if valueA=valueB then trueAST else falseAST
-        | P -> Pair(Literal(Int valueA),Literal(Int valueB))
+    match func with
+    //| P -> Pair()
+    | Add -> A+B |> Int |> Literal //Literal(Int(valueA+valueB))
+    | Sub -> A-B |> Int |> Literal
+    | Mult -> A*B |> Int |> Literal
+    | Div -> A/B |> Int |> Literal
+    | Mod -> A%B |> Int |> Literal
         //| x -> BuiltInFunc(x)
-    | (a,b)->Funcapp(a,b)
+    //| (a,b)->Funcapp(a,b)
 
 let execPairOperation func (a,b) =
     match func with
@@ -100,8 +94,10 @@ let rec exec (exp:AST) :AST =
     let rec betaReduce env exp = 
         let (|BASICS|_|) exp = 
             match exp with
-            | FuncDefExp(name,body,E) -> Some(betaReduce ((name,body)::env) (orderExec false E))
-            | Funcapp(Lambda l,E) -> Some(betaReduce ((l.InputVar,E)::env) (orderExec false l.Body))
+            | FuncDefExp(name,body,E) -> 
+                Some(betaReduce ((name,body)::env) (orderExec false E))
+            | Funcapp(Lambda l,E) -> 
+                Some(betaReduce ((l.InputVar,E)::env) (orderExec false l.Body))
             | Var(name) -> Some(findValue env name)    
             | _ -> None      
 
@@ -114,21 +110,52 @@ let rec exec (exp:AST) :AST =
         | Lambda l -> Lambda {InputVar = l.InputVar; Body = betaReduce env l.Body}
         | x -> x 
 
+    let (|MATCHEQUAL|_|) exp = 
+        match exp with 
+        | (Funcapp(Funcapp(BuiltInFunc(Equal),x),y)) -> Some(x,y) 
+        | _->None
+
+    let (|MATCHMATH|_|) exp = 
+        match exp with 
+        | (Funcapp(Funcapp(BuiltInFunc(Math(_)),x),y)) -> Some(x,y) 
+        | _-> None
+
+    let (|MATCH2ARG|_|) exp = 
+        match exp with
+        | Funcapp(Funcapp(BuiltInFunc(func),x),y) -> Some (func, x, y)
+        | _ -> None
+
+    let (|MATCH2ARG|_|) exp = 
+        match exp with
+        | Funcapp(BuiltInFunc(func),x) -> Some (func, x, y)
+        | _ -> None
+
     let rec evaluate exp =
         let (|BASEFUNCS|_|) exp = 
-            match exp with
-            | Funcapp(Funcapp(BuiltInFunc(func),Literal(Int a)),Literal(Int b)) -> 
-                Some ((Literal (Int a),Literal (Int b)) ||> execArithmetic func)
+            match exp with 
+            | MATCH2ARG (Equal,x,y) -> Null//execEqual x y 
+            | MATCH2ARG (Math op,x,y) -> Null //execFST x y
+            | MATCH2ARG (P,x,y)-> Null
+            | MATCHMATH (x,y) -> Null //execMath x y
+            | 
             | Funcapp(Funcapp(BuiltInFunc(Equal),x),y) -> 
                 if x=y then Some(trueAST) else Some(falseAST)
             | Funcapp(BuiltInFunc(func),Pair(a,b)) -> 
-                Some(execPairOperation func (a,b))
+                (a,b) |> execPairOperation func |> Some
+            | Funcapp(Funcapp(BuiltInFunc(Math func),Literal(Int a)),Literal(Int b)) -> 
+                (a,b) ||> execArithmetic func |> Some// Some ((Literal (Int a),Literal (Int b)) ||> execArithmetic func)
+
+            | Funcapp(BuiltInFunc(Implode), Literal(String s)) -> Some(explode s)
+            | Funcapp(BuiltInFunc(Explode), Pair (a,b)) -> Pair(a,b) |> implode |> Some
             | Funcapp(BuiltInFunc(IsPair),x) -> 
                 match x with | Pair(_) -> Some(trueAST) | _ -> Some(falseAST)
+            | Funcapp(Literal(_), _) -> Some (printf "Cannot apply function to literal")
             | _ -> None  
+
         //printpipe exp
         match exp with
         | BASEFUNCS result -> result
+        | ERRORFUNCS result -> result
         | Funcapp(a,b) -> 
             match Funcapp(evaluate a,evaluate b) with 
             | BASEFUNCS result -> result
@@ -139,11 +166,11 @@ let rec exec (exp:AST) :AST =
     exp |> betaReduce [] |> evaluate
 
 //let f a b = a+b/a in f 3.0 4.0
-let simple0 = Funcapp (Funcapp (BuiltInFunc Add,Literal (Int 2)),Literal (Int 1))
-let simple = Funcapp(Lambda{InputVar = ['x']; Body = Funcapp(Funcapp(BuiltInFunc(Add),Var(['x'])),Literal(Int(1)))},Literal(Int(9)))
+let test0 = Funcapp (Funcapp (BuiltInFunc Add,Literal (Int 2)),Literal (Int 1))
+let test1 = Funcapp(Lambda{InputVar = ['x']; Body = Funcapp(Funcapp(BuiltInFunc(Add),Var(['x'])),Literal(Int(1)))},Literal(Int(9)))
 let simpleMod = (Funcapp(Funcapp(BuiltInFunc(Mod),Literal(Int(120))),Literal(Int(100))))
 let simpleDiv = (Funcapp(Funcapp(BuiltInFunc(Div),simpleMod),Literal(Int(2))))
-let ABbody2 = Funcapp(Funcapp(BuiltInFunc(Add),simpleDiv),Literal(Int(3)))
+let test2 = Funcapp(Funcapp(BuiltInFunc(Add),simpleDiv),Literal(Int(3)))
 
 
 let division = (Funcapp(Funcapp(BuiltInFunc(Div),Var(['b'])),Var(['a'])))
@@ -151,35 +178,40 @@ let addition = (Funcapp(Funcapp(BuiltInFunc(Add),Var(['a'])),division))
 let ABbody = Lambda {InputVar = ['a']; Body = Lambda{InputVar = ['b'];Body = addition}}
 let FBody = Funcapp(Funcapp(Var(['f']),Literal(Int(3))),Literal(Int(27)))
 
-let fig1easy = Funcapp(Funcapp(ABbody,Literal(Int(3))),Literal(Int(27)))
+let test3 = Funcapp(Funcapp(ABbody,Literal(Int(3))),Literal(Int(27)))
 
-let fig1Test = FuncDefExp(['f'],ABbody,FBody) 
+let test4 = FuncDefExp(['f'],ABbody,FBody) 
 // let f a b = a + b/a in f 3 27
 
-let cubeTest = FuncDefExp(['f'],Literal(Int(2)),FBody)
+let test5FBody = Funcapp(Funcapp(BuiltInFunc Mult, Funcapp(Funcapp(BuiltInFunc Mult, Var ['f']),Var['f'])),Var ['f'])
+let test5 = FuncDefExp(['f'],Literal(Int(5)),test5FBody)
 // let f = 2 in f*f*f
 
-let FBodyOrder = Funcapp(Funcapp(Var(['f']),Literal(Int(3))),Funcapp(Funcapp(BuiltInFunc(Mult),Literal(Int(9))),Literal(Int(9))))
-let orderTest = FuncDefExp(['f'],ABbody,FBodyOrder) 
+let test6FBody = Funcapp(Funcapp(Var(['f']),Literal(Int(3))),Funcapp(Funcapp(BuiltInFunc(Mult),Literal(Int(9))),Literal(Int(9))))
+let test6 = FuncDefExp(['f'],ABbody,test6FBody) 
 // let f a b = a + b/a in f 3 9*9
 
-//let closureTest = 
-// let g a b = a + b*a in let f x = g x x in f 3 
+let test7GBody = Lambda{InputVar = ['x'];Body = Funcapp(Var['f'],Var ['x'])}
+let test7FBody =Lambda{InputVar = ['a'];Body = Lambda{InputVar = ['b']; Body = Funcapp(Funcapp(BuiltInFunc Add,Funcapp(Funcapp(BuiltInFunc Mult, Var ['a']),Var['a'])),Var['b'])}} 
+let test7 = FuncDefExp(['f'],test7FBody,FuncDefExp(['g'],test7GBody,(Funcapp(Var ['f'],Literal(Int 3)))))   
+// let g a b = a*a + b in let f x = g x in f 3 
 
-let albertTest = FuncDefExp(['f'],Lambda{InputVar = ['x'];Body = Funcapp(Funcapp(BuiltInFunc Add, Var ['x']),Literal (Int 1))},FuncDefExp(['g'],Lambda{InputVar = ['y']; Body = Funcapp (Funcapp (BuiltInFunc Add, Var ['y']), Literal (Int 2))},(Funcapp(Var ['f'], Funcapp(Var['g'], Literal(Int 3))))))   
+let test8 = FuncDefExp(['f'],Lambda{InputVar = ['x'];Body = Funcapp(Funcapp(BuiltInFunc Add, Var ['x']),Literal (Int 1))},FuncDefExp(['g'],Lambda{InputVar = ['y']; Body = Funcapp (Funcapp (BuiltInFunc Add, Var ['y']), Literal (Int 2))},(Funcapp(Var ['f'], Funcapp(Var['g'], Literal(Int 3))))))   
 //let f x = x+1 in let g y = y+2 in g (f 3)
 
-let albertEasy = FuncDefExp(['f'],Lambda{InputVar = ['x']; Body = Funcapp(Funcapp(BuiltInFunc Add, Var ['x']),Literal (Int 1))},Funcapp (Var['f'],(Funcapp (Var ['f'], Literal(Int 3)))))  
+let test9 = FuncDefExp(['f'],Lambda{InputVar = ['x']; Body = Funcapp(Funcapp(BuiltInFunc Add, Var ['x']),Literal (Int 1))},Funcapp (Var['f'],(Funcapp (Var ['f'], Literal(Int 3)))))  
 // let f x = x+1 in f (f 3)
 
-let result0 = exec simple0
-let result1 = exec simple
-let result2 = exec ABbody2
-let result3 = exec fig1easy
-let result4 = exec fig1Test
-let result5 = exec orderTest
-let result6 = exec albertEasy
-let result7 = exec albertTest
+let result0 = exec test0
+let result1 = exec test1
+let result2 = exec test2
+let result3 = exec test3
+let result4 = exec test4
+let result5 = exec test5
+let result6 = exec test6
+let result7 = exec test7
+let result8 = exec test8
+let result9 = exec test9
 
 //let g a b = a*b*b in let f x y = (g x y)/2 in f 2 5
 
