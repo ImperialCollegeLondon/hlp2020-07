6d5631afbe41a4635d0a7352c498f71b6ae98f22
ej3517 <ej3517@ic.ac.uk>
2020-02-28 11:13:12 +0000

    "Updated version of the combinator run time with the Expecto test"

--- ej3517/CombinatorRuntimeModule.fs
+++ ej3517/CombinatorRuntimeModule.fs
@@ -2,39 +2,58 @@ module CombinatorRuntimeModule
 open System
 
 // Type definition 
+type MathType = 
+   | Add 
+   | Sub
+   | Div
+   | Mult
+   | Mod
+
 type BuiltInType = 
-    | BPlus | BMinus | BTimes | BDivide | BMod
-    | BExplode | BImplode 
-    | BPFst | BPSnd | BIsPair | BP //creates a pair 
-    | BEqual //works for strings ints and nulls 
-    | BTrue | BFalse | BIfThenElse
+    | Math of MathType
+    | BuiltInTest // Haven't looked at it yet
+    | Explode | Implode 
+    | PFst | PSnd | IsPair | P //creates a pair 
+    | Equal //works for strings ints and nulls 
+    | True | False | IfThenElse
     | BS | BK | BI
 
 type AST = 
-    | FuncDefExp of FunDefExpType:(char list) * AST * AST
-    | Lambda of LambdaType:(char list) * Body:AST
+    | FuncDefExp of FuncDefExpType
+    | Lambda of LambdaType//:(char list) * Body:AST
     | Var of char List
     | FuncApp of AST*AST
     | Pair of AST*AST
     | Literal of LitType
     | BFunc of BuiltInType
-    | NULL
+    | Null
+
+and FuncDefExpType = {
+    Name: char list
+    Body: AST
+    Expression: AST
+}
+
+and LambdaType = {
+    InputVar: char list
+    Body: AST
+}
 
 and LitType = 
-    | IntLit of int 
-    | StringLit of string
+    | Int of int 
+    | String of string
 
 //BRACKET ABSTRACTION
 let rec BracketAbstract (lambda : Result<AST,string>) : Result<AST,string> =
     match lambda with
-    | Ok (Lambda(x, FuncApp(E1,E2)))  ->
-        let lambda1 = BracketAbstract (Ok (Lambda(x, E1)))
-        let lambda2 = BracketAbstract (Ok (Lambda(x, E2)))
+    | Ok (Lambda{InputVar = x; Body = FuncApp(E1,E2)})  ->
+        let lambda1 = BracketAbstract (Ok (Lambda{InputVar = x; Body = E1}))
+        let lambda2 = BracketAbstract (Ok (Lambda{InputVar = x; Body = E2}))
         match lambda1, lambda2 with
         | Error r, _ -> Error r
         | _, Error r -> Error r
         | Ok xE1, Ok xE2 -> Ok <| FuncApp( FuncApp( BFunc BS, xE1), xE2)
-    | Ok (Lambda(x, E1)) -> 
+    | Ok (Lambda{InputVar = x; Body = E1}) -> 
         if (Var x) = E1
         then Ok <| BFunc BI
         else Ok <| FuncApp(BFunc BK, E1)
@@ -49,11 +68,11 @@ let rec Abstract (E:Result<AST,string>) : Result<AST,string> =
         | Error r, _ -> Error r
         | _, Error r -> Error r
         | Ok E1, Ok E2 -> Ok <| FuncApp( E1, E2)
-    | Ok (Lambda( x, E1)) -> 
+    | Ok (Lambda{InputVar = x; Body = E1}) -> 
         let aE1' = Abstract (Ok E1)
         match aE1' with
         | Error r -> Error r
-        | Ok E1 -> BracketAbstract <| Ok (Lambda( x, E1))
+        | Ok E1 -> BracketAbstract <| Ok (Lambda{InputVar = x; Body = E1})
     | Ok (Pair(E1,E2)) ->
         let aE1' = Abstract (Ok E1)
         let aE2' = Abstract (Ok E2)
@@ -63,7 +82,7 @@ let rec Abstract (E:Result<AST,string>) : Result<AST,string> =
         | Ok E1, Ok E2 -> Ok <| Pair(E1, E2)
     | Ok (Literal a) -> Ok <| Literal a
     | Ok (BFunc op) -> Ok (BFunc op)
-    | Ok NULL -> Ok NULL
+    | Ok Null -> Ok Null
     | Ok (Var f) -> Ok (Var f)
     | _ -> Error "RUN-TIME ERROR IN THE ABSTRACT"
 
@@ -93,26 +112,26 @@ let rec reducCombinator (E:Result<AST,string>) : Result<AST,string> = // Reducti
     | Ok (Literal x) -> Literal x |> Ok
     | Ok (Var x) -> Var x |> Ok
     | Ok (BFunc f) -> BFunc f |> Ok
-    | Ok NULL -> NULL |> Ok
+    | Ok Null -> Null |> Ok
     | _ -> Error "RUN-TIME ERROR IN EVAL COMBINATOR"
 
 //BUILT-IN FUNCTION
-let explode (s:string) = // Built-In function BExplode and BImplode
+let explode (s:string) = // Built-In function Explode and Implode
     let rec lstToASTLst strLst = 
         match strLst with
-        | [] -> NULL
-        | h :: tl -> Pair(Literal( StringLit h), lstToASTLst tl)
+        | [] -> Null
+        | h :: tl -> Pair(Literal( String h), lstToASTLst tl)
     s |> Seq.toList |> List.map Char.ToString |> lstToASTLst |> Ok
 
 let rec implode (aSTLst:Result<AST,string>) =
     match aSTLst with
-    | Ok (Pair(Literal( StringLit x) , Pair(a,b))) -> 
+    | Ok (Pair(Literal( String x) , Pair(a,b))) -> 
         let y = implode (Ok (Pair(a,b)))
         match y with
         | Error r -> Error r
-        | Ok (Literal( StringLit y')) -> Ok <| Literal( StringLit (x+y'))
+        | Ok (Literal( String y')) -> Ok <| Literal( String (x+y'))
         | _ -> Error "RUN-TIME ERROR : EXPECTED A STRING"
-    | Ok (Pair(Literal( StringLit a) , NULL)) -> Ok <| Literal( StringLit a)
+    | Ok (Pair(Literal( String a) , Null)) -> Ok <| Literal( String a)
     | _ -> Error "RUN-TIME ERROR : EXPECTED A LIST"    
 
 let BuiltMathBool (func':Result<AST,string>) (a':Result<AST,string>) (b':Result<AST,string>) : Result<AST,string> = // FuncApp( FuncApp( BFunc op, a), b)
@@ -121,28 +140,28 @@ let BuiltMathBool (func':Result<AST,string>) (a':Result<AST,string>) (b':Result<
     | Error r, _, _ -> Error r
     | _, Error r, _ -> Error r
     | _, _, Error r -> Error r
-    | Ok (BFunc BPlus), Ok (Literal(IntLit a)), Ok (Literal(IntLit b)) -> Ok (Literal(IntLit (a+b))) // a+b
-    | Ok (BFunc BMinus), Ok (Literal(IntLit a)), Ok (Literal(IntLit b)) ->  Ok (Literal(IntLit (a-b))) // a-b
-    | Ok (BFunc BTimes), Ok (Literal(IntLit a)), Ok (Literal(IntLit b)) -> Ok (Literal(IntLit (a*b))) // a*b
-    | Ok (BFunc BDivide), Ok (Literal(IntLit a)), Ok (Literal(IntLit b)) -> Ok (Literal(IntLit (a/b))) // a/b
-    | Ok (BFunc BMod), Ok (Literal(IntLit a)), Ok (Literal(IntLit b)) -> Ok (Literal(IntLit (((a%b)+b)%b))) // a mod b
-    //Bequal
-    | Ok (BFunc BEqual), a, b -> 
-        if a = b then Ok (BFunc BTrue) else Ok (BFunc BFalse)
+    | Ok (BFunc (Math Add)), Ok (Literal(Int a)), Ok (Literal(Int b)) -> Ok (Literal(Int (a+b))) // a+b
+    | Ok (BFunc (Math Sub)), Ok (Literal(Int a)), Ok (Literal(Int b)) ->  Ok (Literal(Int (a-b))) // a-b
+    | Ok (BFunc (Math Mult)), Ok (Literal(Int a)), Ok (Literal(Int b)) -> Ok (Literal(Int (a*b))) // a*b
+    | Ok (BFunc (Math Div)), Ok (Literal(Int a)), Ok (Literal(Int b)) -> Ok (Literal(Int (a/b))) // a/b
+    | Ok (BFunc (Math Mod)), Ok (Literal(Int a)), Ok (Literal(Int b)) -> Ok (Literal(Int (((a%b)+b)%b))) // a mod b
+    //Equal
+    | Ok (BFunc Equal), a, b -> 
+        if a = b then Ok (BFunc True) else Ok (BFunc False)
     // Church Boolean
-    | Ok (BFunc BTrue), a, _ -> a
-    | Ok (BFunc BFalse), _, b -> b
+    | Ok (BFunc True), a, _ -> a
+    | Ok (BFunc False), _, b -> b
     | _ -> Error "RUN-TIME ERROR IN BuiltMathBool"
 
 let BuiltPair (op:BuiltInType) (x':Result<AST,string>) : Result<AST,string> = // FuncApp( BFunc op, x)
     match op, x' with
     | _, Error r -> Error r
-    | BPFst, Ok (Pair(a,_)) -> Ok a
-    | BPSnd, Ok (Pair(_,b)) -> Ok b
-    | BIsPair, Ok (Pair(_)) -> Ok <| BFunc BTrue
-    | BIsPair, _ -> Ok <| BFunc BFalse
-    | BExplode, Ok (Literal( StringLit a)) -> explode a
-    | BImplode, Ok x -> implode (Ok x)
+    | PFst, Ok (Pair(a,_)) -> Ok a
+    | PSnd, Ok (Pair(_,b)) -> Ok b
+    | IsPair, Ok (Pair(_)) -> Ok <| BFunc True
+    | IsPair, _ -> Ok <| BFunc False
+    | Explode, Ok (Literal( String a)) -> explode a
+    | Implode, Ok x -> implode (Ok x)
     | _ -> Error "RUN-TIME ERROR IN BuiltPair"
 
 // EVALUATION PHASE
@@ -150,14 +169,14 @@ let rec eval (x:Result<AST,string>) : Result<AST,string> =
     let reduceSKI = reducCombinator x
     match reduceSKI with
     | Error r -> Error r
-    | Ok (FuncApp( FuncApp( FuncApp( BFunc BIfThenElse, a), b), c)) -> 
+    | Ok (FuncApp( FuncApp( FuncApp( BFunc IfThenElse, a), b), c)) -> 
         let evalA = a |> Ok |> eval
         match evalA with
         | Error r -> Error r
-        | Ok (BFunc BTrue) -> b |> Ok |> eval
-        | Ok (BFunc BFalse) -> c |> Ok |> eval
+        | Ok (BFunc True) -> b |> Ok |> eval
+        | Ok (BFunc False) -> c |> Ok |> eval
         | _ -> Error "RUN-TIME ERROR : A BOOLEAN WAS EXPECTED BUT WE GOT A ..."
-    | Ok (FuncApp( FuncApp( BFunc BP, a), b)) -> Pair (a,b) |> Ok
+    | Ok (FuncApp( FuncApp( BFunc P, a), b)) -> Pair (a,b) |> Ok
     | Ok (FuncApp( BFunc op, x)) -> 
         let x' = x |> Ok |> eval
         BuiltPair op x'
@@ -168,7 +187,7 @@ let rec eval (x:Result<AST,string>) : Result<AST,string> =
         BuiltMathBool func' a' b'
     | Ok (Literal a )-> Literal a |> Ok
     | Ok (Pair(a,b)) -> Pair(a,b) |> Ok
-    | Ok NULL -> Ok NULL
+    | Ok Null -> Ok Null
     | Ok (BFunc f) -> BFunc f |> Ok
     | _ -> Error "RUN-TIME ERROR IN THE EVALUATION PHASE"
 
@@ -199,23 +218,22 @@ let rec substitudeVarWithExpression (f: char list) (e1: AST) (e2: Result<AST,str
             | Var g -> Var g |> Ok
             | Literal a -> Literal a |> Ok
             | BFunc op -> BFunc op |> Ok
-            | NULL -> NULL |> Ok
+            | Null -> Null |> Ok
             | _ -> Error "RUN-TIME ERROR IN substitudeVarWithExpression 1"
     | _ -> Error "RUN-TIME ERROR IN substitudeVarWithExpression 2"
 
-let reductionAST (y:AST) : Result<AST,string> =
+let Reduce (y:AST) : Result<AST,string> =
     let rec reduceFuncTree (x:Result<AST,string>) : Result<AST,string> =
         match x with
-        | Ok (FuncDefExp( f, E1, E2)) ->
+        | Ok (FuncDefExp{Name =  f; Body = E1; Expression = E2}) ->
             let evalE1 = E1 |> Ok |> reduceFuncTree
             let evalE2 = E2 |> Ok |> reduceFuncTree
             match evalE1 with 
             | Ok evalE1' -> 
                 substitudeVarWithExpression f evalE1' evalE2
-            | _ -> Error "RUN-TIME ERROR IN reductionAST 1"
-        | Ok (Lambda( x, E1)) ->
-            (Lambda( x, E1)) |> Ok |> Abstract
+            | _ -> Error "RUN-TIME ERROR IN Reduce 1"
+        | Ok (Lambda{InputVar = x; Body = E1}) ->
+            (Lambda{InputVar = x; Body = E1}) |> Ok |> Abstract
         | Ok (FuncApp(E1,E2)) -> Ok (FuncApp(E1,E2))
-        | _ -> Error "RUN-TIME ERROR IN reductionAST 2"
+        | _ -> Error "RUN-TIME ERROR IN Reduce 2"
     y |> Ok |> reduceFuncTree |> eval
-
