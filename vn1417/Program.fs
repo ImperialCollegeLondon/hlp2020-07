5b790e37f5b25ca9b7e0e51d8077a2219ee2fa5d
Vlad <vn1417@ic.ac.uk>
2020-03-05 22:04:12 +0000

    Update Program.fs

--- vn1417/Program.fs
+++ vn1417/Program.fs
@@ -7,12 +7,27 @@ open System.IO
 let print x =
     printfn "%A" x
 
+
+//**BUG**
+//the keyword if shadows anything that starts with if -> ifinit -> keyword "if"; Other "init" !. FIX
 let testDescriptions = [
-    ("match end match with semicolon inside",tokenize "match ; \n endmatch",[Keyword "match";SpaceLit;Keyword ";";SpaceLit;Newline;SpaceLit;Keyword "endmatch"],"tokenize match ; \\n endmatch")
-    ("match",tokenize "\n match other \n",[Newline;SpaceLit;Keyword "match";SpaceLit;Other "other";SpaceLit;Newline],"tokenize '\\n match other \\n' -> [Newline;SpaceLit;Keyword \"match\";SpaceLit;Other \"other\";SpaceLit;Newline]")
+    //Note how it's inserting a space; Is it necesarilly bad if it fails anyway?
+    ("match if <", tokenize "match if <", [Keyword "match";Keyword "if";Unexpected "< "],"tokenize match if <")
+    //Note how it's inserting a space; Is it necesarilly bad if it fails anyway?
+    ("< match end if", tokenize "< match end if", [Unexpected "< match end if "],"tokenize < match end if")
+    ("endmatchx \n\n", tokenize "endmatchx \n\n",[Other "endmatchx"; Newline; Newline],"endmatchx \\n\\n")
+    ("Semicolon with space",tokenize "; ", [Keyword ";"],"tokenize ; _")
+    ("Single semicolon", tokenize ";", [Keyword ";"],"tokenize ;")
+    ("Match with space",tokenize "match ", [Keyword "match"],"tokenize match _")
+    ("Single match",tokenize "match", [Keyword "match"],"tokenize match")
+    ("ifinit elseinit theninit finit if else then fi other",tokenize "ifinit elseinit theninit finit if else then fi other",[Other "ifinit"; Other "elseinit"; Other "theninit";  Other "finit"; Keyword "if"; Keyword "else"; Keyword "then";  Keyword "fi"; Other "other"],"tokenize match 'ifinit elseinit theninit finit if else then fi other'")
+    ("f -2", tokenize "f -2", [Other "f"; IntegerLit -2],"Substraction vs Application")
+    ("f - 2", tokenize "f - 2", [Other "f"; SubToken; IntegerLit 2],"Substraction vs Application")
+    ("match end match with semicolon inside",tokenize "match ; \n endmatch",[Keyword "match";Keyword ";";Newline;Keyword "endmatch"],"tokenize match ; \\n endmatch")
+    ("match",tokenize "\n match other \n",[Newline;Keyword "match";Other "other";Newline],"tokenize '\\n match other \\n' -> [Newline;SpaceLit;Keyword \"match\";SpaceLit;Other \"other\";SpaceLit;Newline]")
     ("No Input",tokenize "", [NoInput],"tokenize \"\" -> NoInput")
     ("Single new line lit",tokenize "\n",[Newline],"tokenize \\n -> Newline")
-    ("Newline lit in the middle",tokenize "; \n ;",[Keyword ";";SpaceLit;Newline;SpaceLit ;Keyword ";"],"tokenize \n -> newlineLit")
+    ("Newline lit in the middle",tokenize "; \n ;",[Keyword ";";Newline ;Keyword ";"],"tokenize \n -> newlineLit")
     ("Single Integer Lit",tokenize "21", [IntegerLit 21] ,"tokenize 21 -> IntegerLit 21");
     ("Single Decimal Lit",tokenize "21.2",[DecimalLit 21.2],"tokenize 21.2 -> DecimalList 21.2")
     ("Zero integer", tokenize "0", [IntegerLit 0], "tokenize 0 -> IntegerLit 0")
@@ -24,42 +39,24 @@ let testDescriptions = [
     ("All symbols", tokenize "-> - / * + let ( ) [ ] 21.2 -21.2 21 -21 = \n 0xffaa other \"Hello There \"",
                                                 [
                                                     RightArrow
-                                                    SpaceLit
                                                     SubToken
-                                                    SpaceLit
                                                     DivToken
-                                                    SpaceLit
                                                     MultToken
-                                                    SpaceLit
                                                     AddToken
-                                                    SpaceLit
                                                     Let
-                                                    SpaceLit
                                                     OpenRoundBracket
-                                                    SpaceLit
                                                     CloseRoundBracket
-                                                    SpaceLit
                                                     OpenSquareBracket
-                                                    SpaceLit
                                                     CloseSquareBracket
-                                                    SpaceLit
                                                     DecimalLit 21.2
-                                                    SpaceLit
                                                     DecimalLit -21.2
-                                                    SpaceLit
                                                     IntegerLit 21
-                                                    SpaceLit
                                                     IntegerLit -21
-                                                    SpaceLit
                                                     EqualToken
-                                                    SpaceLit
                                                     Newline
-                                                    SpaceLit
                                                     HexLit "0xffaa"
-                                                    SpaceLit
-                                                    Other "other"
-                                                    SpaceLit
-                                                    StringLit "\"Hello There \""                                         
+                                                    Other "other" 
+                                                    StringLit "\"Hello There \""   
                                                 ],                                         
                                                 "tokenize -> - / * + let ( ) [ ] 21.2 -21.2 21 -21 = \\n 0xffaa other \"Hello There \"")
     ]
@@ -83,38 +80,7 @@ let mList = [1;2;3]
 let main argv =
     //print <| tokenize "-> - / * + let ( ) [ ] 21.2 -21.2 21 -21 = 0xffaa other \"Hello There \""
     runTestsInAssembly defaultConfig [||] |> ignore
-    (*let readLines =
-              "/Users/vladnistor/Documents/dev/hlp2020-07/vn1417/Input.txt"
-              |> File.ReadAllLines
-              |> Array.toList
-              
-    print readLines
-    *)
-    //print <| Seq.toList "\n"
-    //print <| tokenize "= \n 0xffaa other \"Hello There \""
-    //Tests.runTests defaultConfig properties |> ignore
-    //print <| tokenize "21.2"
-    //print "Hi "
+    //print <| tokenize "if a=b"
+    //print <| mdict
+    //print <| tokenize " ifinit match other "
     0
-    
-
-    
-    
-(*
-
-expected:
-[RightArrow; SpaceLit; SubToken; SpaceLit; DivToken; SpaceLit; MultToken;
- SpaceLit; AddToken; SpaceLit; Let; SpaceLit; OpenRoundBracket; SpaceLit;
- CloseRoundBracket; SpaceLit; OpenSquareBracket; SpaceLit; CloseSquareBracket;
- SpaceLit; DecimalLit 21.2; SpaceLit; DecimalLit -21.2; SpaceLit; IntegerLit 21;
- SpaceLit; IntegerLit -21; SpaceLit; EqualToken; SpaceLit; HexLit "0xffaa";
- SpaceLit; Other "other"; SpaceLit; StringLit ""Hello There ""]
-  actual:
-[RightArrow; SubToken; DivToken; MultToken; AddToken; Let; OpenRoundBracket;
- CloseRoundBracket; OpenSquareBracket; CloseSquareBracket; DecimalLit 21.2;
- DecimalLit -21.2; IntegerLit 21; IntegerLit -21; EqualToken; HexLit "0xffaa";
- Other "other"; StringLit ""Hello There ""]
-
-                                             
-*)
-
